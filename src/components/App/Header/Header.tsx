1d65dda71172006a54308aba3e96f5e25432b210
Laura Feugene <laura.feugene.gmail.com>
2023-06-06 11:12:18 +0200

    style: :art: change to the right side with a logo

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -30,17 +30,17 @@ function Header() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-end">
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    src="src\assets\logo-ovoyage.svg"
+                    alt="O'Voyage"
                   />
                   <img
                     className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    src="src\assets\logo-ovoyage.svg"
+                    alt="O'Voyage"
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
