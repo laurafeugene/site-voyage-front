27487ea63984b112f1d9a665fc225e5d4ff6044e
Laura Feugene <laura.feugene.gmail.com>
2023-06-06 17:11:48 +0200

    style: fix style footer with default colors

--- src/components/App/Footer/Footer.tsx
+++ src/components/App/Footer/Footer.tsx
@@ -23,7 +23,7 @@ function Footer() {
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:flex flex-wrap">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-lightest hover:bg-gray-700 hover:text-lightest focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightest">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-lightest hover:bg-darkest-700 hover:text-lightest focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lightest">
                   {/*<span className="sr-only">Open main menu</span>
                    {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -42,7 +42,7 @@ function Footer() {
                         className={classNames(
                           item.current                             
                           ? 'bg-lightest text-darkest'
-                          : 'text-lightest hover:bg-gray-700 hover:text-lightest',
+                          : 'text-lightest hover:bg-darkest-700 hover:text-lightest',
                         'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -58,7 +58,7 @@ function Footer() {
                 {/* Profile dropdown */}{/*
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="flex rounded-full bg-darkest-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-darkest-800">
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
@@ -81,7 +81,7 @@ function Footer() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-darkest-100' : '', 'block px-4 py-2 text-sm text-darkest-700')}
                           >
                             Your Profile
                           </a>
@@ -91,7 +91,7 @@ function Footer() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-darkest-100' : '', 'block px-4 py-2 text-sm text-darkest-700')}
                           >
                             Settings
                           </a>
@@ -101,7 +101,7 @@ function Footer() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-darkest-100' : '', 'block px-4 py-2 text-sm text-darkest-700')}
                           >
                             Sign out
                           </a>
@@ -122,7 +122,7 @@ function Footer() {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? 'bg-darkest-900 text-white' : 'text-darkest-300 hover:bg-darkest-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
