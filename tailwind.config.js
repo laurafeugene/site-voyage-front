673fac6d44ea2588bdde07f973a101cc10c37f93
Laura Feugene <laura.feugene.gmail.com>
2023-06-06 16:44:26 +0200

    style: :art: add borderRadius class to tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       colors: {
         'darkest': '#3B4B59',
         'warm': '#D98D62',
         'medium':'#F2B999',
         'cool': '#A68072',     
         'lightest': '#F2D9D0',
+    },
+    fontFamily: {},
+    borderRadius: {
+      'none': '0', 
+      'sm': '0.125rem',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'xl': '0.75rem',
+      '2xl': '1rem', 
 
     },
-  },
+  extend: {},
 },
-  plugins: [require("daisyui", "tailwindcss/forms")],
+  plugins: [require("daisyui")],
 };
