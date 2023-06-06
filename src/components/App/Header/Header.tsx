25eadfe6aacce524ee30b94085e1e4380f7d5db5
Laura Feugene <laura.feugene.gmail.com>
2023-06-06 16:59:34 +0200

    style: :art: apply default style to header

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -71,7 +71,7 @@ function Header() {
                     <Menu.Button className="flex rounded-full bg-darkest text-sm focus:outline-none focus:ring-2 focus:ring-lightest focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
                       <img
-                        className="h-8 w-8 rounded-full"
+                        className="h-8 w-8 rounded-2xl"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
