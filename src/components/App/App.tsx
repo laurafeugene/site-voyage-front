a2c5e9388516372803580d4d6608ab90d22c3190
ManonDervieux <manon.dervieux1992@gmail.com>
2023-06-07 10:49:51 +0200

    feat: faq_page
    faq page with visual header and footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,15 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Home from "./Home/Home";
+import Faq from "./Faq/Faq";
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <Faq />
       <Footer/>
-
     </>
   );
 }
---
+++ src/components/App/Faq/Faq.tsx
@@ -0,0 +1,74 @@
+function Faq() {
+    return (
+        
+        <div className="px-20 pb-40">
+
+            
+            <h1 className="py-20 font-bold text-2xl text-darkest text-center  ">FAQ</h1>
+            
+
+            <div className="py-1">
+                <div className="collapse collapse-arrow bg-darkest">
+                    <input type="radio" name="my-accordion-2"  /> 
+                    <div className="collapse-title text-xl font-medium text-lightest">
+                            Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-lightest"> 
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="py-1">
+                <div className="collapse collapse-arrow bg-darkest">
+                    <input type="radio" name="my-accordion-2"  /> 
+                    <div className="collapse-title text-xl font-medium text-lightest">
+                            Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-lightest"> 
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="py-1">
+                <div className="collapse collapse-arrow bg-darkest">
+                    <input type="radio" name="my-accordion-2"  /> 
+                    <div className="collapse-title text-xl font-medium text-lightest">
+                            Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-lightest"> 
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="py-1">
+                <div className="collapse collapse-arrow bg-darkest">
+                    <input type="radio" name="my-accordion-2"  /> 
+                    <div className="collapse-title text-xl font-medium text-lightest">
+                            Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-lightest"> 
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="py-1">
+                <div className="collapse collapse-arrow bg-darkest">
+                    <input type="radio" name="my-accordion-2"  /> 
+                    <div className="collapse-title text-xl font-medium text-lightest">
+                            Click to open this one and close others
+                    </div>
+                    <div className="collapse-content text-lightest"> 
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Faq;
