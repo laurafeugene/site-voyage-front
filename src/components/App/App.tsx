e22a36b14faafce7f5ae91a1e019072090918276
Val <pankakekaribou@hotmail.fr>
2023-06-07 14:58:50 +0200

    fix: git_debug

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,13 @@
 import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Home from "./Home/Home";
-import ConnectionForm from './Connection/connection';
+// import Footer from './Footer/Footer';
+import Home from './Home/Home';
 
 function App() {
   return (
     <>
-      {/* <Header />
+      <Header />
       <Home />
-      <Footer/> */}
-
-
+      {/* <Footer /> */}
     </>
   );
 }
