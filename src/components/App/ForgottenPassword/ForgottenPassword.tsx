d62d5d344ece1dd3290c691f2d06e0c239d46023
Laura Feugene <laura.feugene.gmail.com>
2023-06-09 15:12:20 +0200

    :feat: ajout du formulaire de mot de passe oublié avec champ obligatoire

--- src/components/App/ForgottenPassword/ForgottenPassword.tsx
+++ src/components/App/ForgottenPassword/ForgottenPassword.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function PasswordResetForm() {
   const [email, setEmail] = useState('');
@@ -17,24 +17,37 @@ function PasswordResetForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Mot de passe oublié</h2>
-      <p>
-        Veuillez saisir votre adresse e-mail pour réinitialiser votre mot de
-        passe.
-      </p>
-      <div>
-        <label htmlFor="email">Adresse e-mail :</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="w-auto">
+        <form onSubmit={handleSubmit}>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-darkest">
+            REINITIALISER VOTRE MOT DE PASSE
+          </h2>
+          <p className="mt-10 text-center text-sm tracking-tight pb-3 text-darkest max-w-md">
+            Si vous avez oublié votre mot de passe, veuillez saisir votre
+            adresse e-mail enregistrée. Nous vous enverrons un lien pour
+            réinitialiser votre mot de passe.
+          </p>
+          <div>
+            <input
+              type="email"
+              placeholder="L'adresse email ne peut pas être vide."
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+              className="block w-full rounded-md border-0 text-center p-1.5 text-darkest-900 text-sm shadow-sm ring-1 ring-inset ring-darkest-300 placeholder:text-darkest-400 mb-3 max-w-md focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-darkest px-3 py-1.5 text-sm font-semibold leading-6 text-lightest max-w-md shadow-sm "
+          >
+            Réinitialiser le mot de passe
+          </button>
+        </form>
       </div>
-      <button type="submit">Réinitialiser le mot de passe</button>
-    </form>
+    </div>
   );
 }
 
