e6ffe99a29cbace0046140442b760fb608b8948e
ManonDervieux <manon.dervieux1992@gmail.com>
2023-06-09 10:44:34 +0200

    refactor: Add Avatar group with counter

--- src/components/App/GeneralTravel/GeneralTravel.tsx
+++ src/components/App/GeneralTravel/GeneralTravel.tsx
@@ -1,11 +1,36 @@
 function GeneralTravel() {
+
+
     return(
         <main>
             <div className="flex w-full">
                 <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Mon Voyage (destination)</div>
                 <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Début du voyage :</div>
                 <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Fin du voyage :</div>
-                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Participants</div>
+                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">
+                <div className="avatar-group -space-x-6">
+                    <div className="avatar">
+                        <div className="w-12">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <div className="avatar">
+                        <div className="w-12">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <div className="avatar">
+                        <div className="w-12">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <div className="avatar placeholder">
+                        <div className="w-12 bg-neutral-focus text-neutral-content">
+                        <span>+99</span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
                 <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Budget</div>
             </div>  
         </main>
