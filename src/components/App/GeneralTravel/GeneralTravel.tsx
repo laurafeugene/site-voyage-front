8dbf5e89cafb9ad97949ee61ec74cf8790d7e012
ManonDervieux <manon.dervieux1992@gmail.com>
2023-06-09 09:20:56 +0200

    refactor: base code with tailwing style

--- src/components/App/GeneralTravel/GeneralTravel.tsx
+++ src/components/App/GeneralTravel/GeneralTravel.tsx
@@ -1,8 +1,14 @@
 function GeneralTravel() {
     return(
-        <div>
-            <h1>GeneralTravel</h1>
-        </div>
+        <main>
+            <div className="flex w-full">
+                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Mon Voyage (destination)</div>
+                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Début du voyage :</div>
+                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Fin du voyage :</div>
+                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Participants</div>
+                <div className="grid h-20 flex-grow card bg-lightest rounded-box place-items-center mx-2 my-2">Budget</div>
+            </div>  
+        </main>
     );
 }
 
