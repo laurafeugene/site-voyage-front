3c06a4812a3355aac8907042e19a0489153d5888
ManonDervieux <manon.dervieux1992@gmail.com>
2023-06-12 14:17:16 +0200

    refactor: add responsive in general Travel Page

--- src/components/App/GeneralTravel/GeneralTravelMain.tsx
+++ src/components/App/GeneralTravel/GeneralTravelMain.tsx
@@ -1,19 +1,19 @@
 function GeneralTravelMain() {
     return(
-        <div className="flex flex-col sm:flex sm:flex-wrap space-x-1">
+        <div className="flex w-full sm:flex-col space-x-1">
 
-            <div className="flex flex-wrap">
+            <div className="flex md:flex-row w-full sm:flex-col">
 
                 {/* contenant les activités du voyage */}
 
-                <div className="flex flex-1 flex-col w-full m-2"> {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives au activités dans l'ordre d'arrivée*/}
+                <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2"> {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives au activités dans l'ordre d'arrivée*/}
                     <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">Ski</div> 
                     <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">patin à glace</div>
                 </div>
 
                 {/* contenant le budget du voyage */}
                 
-                <div className="flex flex-1 flex-col w-full m-2"> 
+                <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2"> 
                     <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">Budget avec décompte</div>
                 </div>
 
@@ -22,7 +22,7 @@ function GeneralTravelMain() {
 
                 {/* contenant les prochaines étapes du voyage */}
 
-                <div className="flex flex-1 flex-col w-full m-2"> {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives aux prochaines étapes (itinéraires) du voyage */}
+                <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2"> {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives aux prochaines étapes (itinéraires) du voyage */}
                     <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">Alpes</div> 
                     <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">Genève</div>
                 </div>
