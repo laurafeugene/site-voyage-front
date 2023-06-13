19462bd1cf585d9941f3fe5b991436a0cd27a8c6
Laura Feugene <laura.feugene.gmail.com>
2023-06-13 21:40:53 +0200

    add cookies into header

--- src/components/App/Header/Header.tsx
+++ src/components/App/Header/Header.tsx
@@ -164,7 +164,7 @@ function Header() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/"
                             className={classNames(
                               active ? 'bg-gray-100' : '',
                               'block px-4 py-2 text-sm text-gray-700'
