8253e65267114b732e6d4bb14856534abd1432ec
ManonDervieux <manon.dervieux1992@gmail.com>
2023-06-15 10:23:34 +0200

    perf: Home Page design

--- src/components/App/Home/Home.tsx
+++ src/components/App/Home/Home.tsx
@@ -1,8 +1,16 @@
 import TravelForm from './TravelForm/TravelForm';
+import Slider from './Slider/Slider';
+import Presentation from './presentation/presentation';
+import Reviews from './reviews/Reviews';
 
 function Home() {
   return (
+    <>
     <TravelForm />
+    <Slider />
+    <Presentation />    
+    <Reviews />
+    </>
   );
 }
 
