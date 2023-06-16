bcbff2f4f932011c7613ad453ffc470c084283ab
Val <pankakekaribou@hotmail.fr>
2023-06-16 10:45:46 +0200

    feat: travel hard data

---
+++ src/data/travelData.js
@@ -0,0 +1,23 @@
+export default {
+  travels: [
+    {
+      title: 'Mon Super Voyage',
+      from: 'France',
+      to: 'Italie',
+      departureDate: '2024-01-01',
+      arrivalDate: '2024-04-01',
+      budget: 1312,
+      attendees: [
+        {
+          firstname: 'Valentin',
+          lastname: 'Desoeuvre',
+          email: 'valentin@ovoyage.com',
+          id: 1,
+        },
+      ],
+      id: 1,
+      numberOfAttendees: 5,
+      organizerId: 5,
+    },
+  ],
+};
