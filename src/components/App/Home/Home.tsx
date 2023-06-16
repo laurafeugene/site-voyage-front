55a791834fcffd1184e44c86aa43d95dc1b0c8f0
Laura Feugene <laura.feugene.gmail.com>
2023-06-16 12:17:55 +0200

    :feat: fix import into home componenant

--- src/components/App/Home/Home.tsx
+++ src/components/App/Home/Home.tsx
@@ -2,7 +2,6 @@ import TravelForm from './TravelForm/TravelForm';
 import Slider from './Slider/Slider';
 import Presentation from './presentation/presentation';
 import Reviews from './reviews/Reviews';
-import GlobalPage from './TravelForm/GlobalPage';
 import TitleTravelHomePage from './TravelForm/TilteHomePage';
 
 function Home() {
