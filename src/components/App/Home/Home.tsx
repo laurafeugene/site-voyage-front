676babf9653c6ef299611ae9116defe72640994a
Laura Feugene <laura.feugene.gmail.com>
2023-06-16 12:17:15 +0200

    :feat: change home componenant to create a new componant for title

--- src/components/App/Home/Home.tsx
+++ src/components/App/Home/Home.tsx
@@ -2,14 +2,17 @@ import TravelForm from './TravelForm/TravelForm';
 import Slider from './Slider/Slider';
 import Presentation from './presentation/presentation';
 import Reviews from './reviews/Reviews';
+import GlobalPage from './TravelForm/GlobalPage';
+import TitleTravelHomePage from './TravelForm/TilteHomePage';
 
 function Home() {
   return (
     <>
-    <TravelForm />
-    <Slider />
-    <Presentation />    
-    <Reviews />
+      <TitleTravelHomePage />
+      <TravelForm />
+      <Slider />
+      <Presentation />
+      <Reviews />
     </>
   );
 }
---
+++ src/components/App/Home/TravelForm/TilteHomePage.tsx
@@ -0,0 +1,8 @@
+function TitleTravelHomePage() {
+  return (
+    <legend className="flex flex-col justify-center items-center pt-10 bg-medium p-2 text-2xl">
+      Préparez votre voyage dès maintenant !
+    </legend>
+  );
+}
+export default TitleTravelHomePage;
