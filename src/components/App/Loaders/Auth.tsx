be11e543d6a4ae4bfc5b1ea2f7352ad8e2d0aaf1
Laura Feugene <laura.feugene.gmail.com>
2023-06-21 15:02:02 +0200

    :feat: add usedispatch into auth

--- src/components/App/Loaders/Auth.tsx
+++ src/components/App/Loaders/Auth.tsx
@@ -1,7 +1,11 @@
-import { stores } from '../../../store/reducers/user';
+import { stores, setIsLogged } from '../../../store/reducers/user';
+import { useAppDispatch } from '../../../hooks/redux';
 
 function getToken() {
   const loggedIn = stores.getState().userReducer.isLogged;
+  const dispatch = useAppDispatch();
+  dispatch(setIsLogged(true));
+
   console.log(loggedIn);
   return null;
 }
