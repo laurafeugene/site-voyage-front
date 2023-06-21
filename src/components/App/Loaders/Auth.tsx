f87fc82e05c8b5a2fa6cbeadc0ebae96c2c65575
Laura Feugene <laura.feugene.gmail.com>
2023-06-21 13:44:53 +0200

    :feat: add auth file

---
+++ src/components/App/Loaders/Auth.tsx
@@ -0,0 +1,31 @@
+import jwtDecode from 'jwt-decode';
+import Cookies from 'js-cookie';
+import { redirect } from 'react-router';
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../../../hooks/redux';
+import { refreshToken, setIsLoggedTrue } from '../../../store/reducers/user';
+
+function getToken() {
+  const dispatch = useAppDispatch();
+  const isLogged = useAppSelector((state) => state.user.isLogged);
+  useEffect(() => {
+    if (!isLogged) {
+      const accessToken = Cookies.get('accessToken');
+      const refreshToken = Cookies.get('refreshToken');
+      if (accessToken) {
+        const decodedToken = jwtDecode(accessToken);
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (decodedToken.exp >= currentTime) {
+          // Redirection vers la page de connexion
+          redirect('/connexion');
+          // Appel à dispatch pour renouveler le token
+          dispatch(refreshToken(refreshToken));
+          // Appel à dispatch pour définir isLogged à true
+          dispatch(setIsLoggedTrue());
+        }
+      }
+    }
+  }, [dispatch, isLogged]);
+  return null;
+}
+export default getToken;
