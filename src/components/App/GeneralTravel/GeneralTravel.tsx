bc8d655a860b9650e2011e214b7947a49bf5b13a
Val <pankakekaribou@hotmail.fr>
2023-06-22 12:07:11 +0200

    refactor: GeneralTravel > clean code

--- src/components/App/GeneralTravel/GeneralTravel.tsx
+++ src/components/App/GeneralTravel/GeneralTravel.tsx
@@ -1,48 +1,44 @@
-import RecapForm from "./RecapForm";
-import { RecapFormProps, getRecapForm } from "../../../store/reducers/dataForm";
-import NavDay from "./NavDay";
-import GeneralTravelMain from "./GeneralTravelMain";
-import { useEffect, useState} from "react";
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
 
+import { RecapFormProps, getRecapForm } from '../../../store/reducers/dataForm';
+
+import RecapForm from './RecapForm';
+import NavDay from './NavDay';
+import GeneralTravelMain from './GeneralTravelMain';
 
 function GeneralTravel() {
+  const [numberOfDays, setNumberOfDays] = useState<number>(0);
   const [recapForm, setRecapForm] = useState<RecapFormProps>({
     arrivalDate: '',
     departureDate: '',
     budget: '',
     title: '',
   });
-    const [numberOfDays, setNumberOfDays] = useState<number>(0);
 
+  useEffect(() => {
+    getRecapForm(1).then((data) => {  // remplacer le 2 par l'id de l'url du voyage
+      setRecapForm(data);
+    });
+  }, []);
 
-    useEffect(() => {
-        getRecapForm(1).then((data) => {  // remplacer le 2 par l'id de l'url du voyage
-            setRecapForm(data);
-        });
-    }, []);
+  useEffect(() => {
+    if (recapForm.arrivalDate && recapForm.departureDate) {
+      const arrivalDay = dayjs(recapForm.arrivalDate);
+      const departureDay = dayjs(recapForm.departureDate);
+      const duration = Math.abs(departureDay.diff(arrivalDay, 'day')); // Utilisation de Math.abs() pour obtenir la valeur absolue
+      console.log('Nombre de jours entre les dates : ', duration);
+      setNumberOfDays(duration);
+    }
+  }, [recapForm]);
 
-    useEffect(() => {
-        if (recapForm.arrivalDate && recapForm.departureDate) {
-            const arrivalDay = dayjs(recapForm.arrivalDate);
-            const departureDay = dayjs(recapForm.departureDate);
-            const duration = Math.abs(departureDay.diff(arrivalDay, 'day')); // Utilisation de Math.abs() pour obtenir la valeur absolue
-            console.log("Nombre de jours entre les dates : ", duration);
-            setNumberOfDays(duration);
-        }
-    }, [recapForm]);
-    
-
-    
-
-    return(
-        <>
-        <RecapForm arrivalDate={recapForm.arrivalDate} departureDate={recapForm.departureDate} budget={recapForm.budget} title={recapForm.title}/> 
-        <NavDay numberOfDays={numberOfDays} />
-        <GeneralTravelMain />
-        </>
-    );
-
+  return (
+    <>
+      <RecapForm arrivalDate={recapForm.arrivalDate} departureDate={recapForm.departureDate} budget={recapForm.budget} title={recapForm.title}/> 
+      <NavDay numberOfDays={numberOfDays} />
+      <GeneralTravelMain />
+    </>
+  );
 }
 
 export default GeneralTravel;
