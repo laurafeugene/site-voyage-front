a00262eeee6db844f26697f1ffedeb05a64aacf2
Laura Feugene <laura.feugene.gmail.com>
2023-06-22 11:06:06 +0200

    :feat: add function to check accesstoken

--- src/components/App/Loaders/Auth.tsx
+++ src/components/App/Loaders/Auth.tsx
@@ -3,24 +3,50 @@ import axios from 'axios';
 import { redirect } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 
-function getToken() {
-  const accessToken = Cookies.get('accessToken');
-  if (!accessToken) {
-    // mettre la logique du token ici // faire pleins de petites fonctions
+// Fonction pour décoder le token via JWT Decode
+function decodeToken(token) {
+  try {
+    const decodedToken = jwtDecode(token);
+    return decodedToken;
+  } catch (error) {
+    console.error(error);
     return null;
   }
-  axios.defaults.headers.common.Authorization = `${accessToken}`;
-  return accessToken;
 }
 
+// Fonction pour rediriger vers la page de connexion
+function redirectToLogin() {
+  redirect('/connexion');
+}
+
+// Fonction pour charger le token
+function getToken() {
+  const accessToken = Cookies.get('accessToken');
+
+  if (accessToken) {
+    // Décoder le token avec JWT Decode
+    const decodedToken = decodeToken(accessToken);
+    if (decodedToken) {
+      axios.defaults.headers.common.Authorization = `${accessToken}`;
+      return accessToken;
+    }
+    // Si le token est invalide ou n'a pas pu être décodé
+    redirectToLogin();
+  }
+
+  return null;
+}
+
+// Composant TokenLoader
 export default function TokenLoader() {
   return getToken();
 }
 
+// Fonction pour charger l'authentification
 export function authLoader() {
   const token = getToken();
   if (!token) {
-    return redirect('/connexion');
+    redirectToLogin();
   }
   return null;
 }
