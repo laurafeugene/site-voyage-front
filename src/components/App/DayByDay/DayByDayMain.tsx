9f176e00a8900cf89c920b249097914186cee4e6
Laura Feugene <laura.feugene.gmail.com>
2023-06-23 11:09:17 +0200

    :fix: remove image from day to day trip

--- src/components/App/DayByDay/DayByDayMain.tsx
+++ src/components/App/DayByDay/DayByDayMain.tsx
@@ -53,48 +53,6 @@ function DayByDayMain(props: ActivityProps) {
           </div>
         </div>
       </div>
-      {/* carrousel des photos souvenirs du voyage */}
-      <div className="flex flex-wrap justify-center item center">
-        <div className="carousel max-w-xl my-2">
-          {/* carrousel d'image pour les photos du voyage à modifier pour chercher les images de la base donnée et ajouter une fonction pour que si pas de photo, afficher un message "Ajoutez vos souvenirs de voyage" */}
-          <div id="item1" className="carousel-item w-full">
-            <img
-              src="../src/assets/foxtail-grass-g143d33576_1280.jpg"
-              className="w-full object-contain"
-            />
-          </div>
-          <div id="item2" className="carousel-item w-full">
-            <img
-              src="../src/assets/butterfly-g87b0c801f_1280.jpg"
-              className="w-full object-contain"
-            />
-          </div>
-        </div>
-
-        <div className="flex justify-center w-full py-2 gap-2 m-2">
-          <a
-            href="#item1"
-            className="btn btn-xs bg-darkest text-lightest font-semibold hover:bg-lightest hover:text-darkest"
-          >
-            1
-          </a>
-          <a
-            href="#item2"
-            className="btn btn-xs bg-darkest text-lightest font-semibold hover:bg-lightest hover:text-darkest"
-          >
-            2
-          </a>
-        </div>
-
-        {/* bouton ajoutez des photos souvenirs */}
-
-        <div className="form-control w-full max-w-xs m-2">
-          <input
-            type="file"
-            className="file-input file-input-bordered file-input-darkest file-input-sm w-full max-w-xs"
-          />
-        </div>
-      </div>
     </div>
   );
 }
