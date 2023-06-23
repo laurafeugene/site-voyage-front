899e9c7ab38ad5cc69bf5a0472441814f77036f6
Val <pankakekaribou@hotmail.fr>
2023-06-23 11:33:10 +0200

    feat: activity reducer > add activity query

--- src/store/reducers/activity.ts
+++ src/store/reducers/activity.ts
@@ -1,4 +1,5 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createAsyncThunk, createReducer } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 interface ActivityState {
   activity: [];
@@ -8,7 +9,32 @@ const initialState: ActivityState = {
   activity: [],
 };
 
-export const addActivity = createAction('activity/add');
+const addActivityQuery = `
+  mutation Mutation {
+    createActivity(createActivityInput {
+      name: "${name}"
+      price: "${price}"
+      location: "${location}"
+      members: "${members}"
+      time: "${time}"
+      date: "${date}"
+      travelId: "${travelId}"
+      categoryId: "${categoryId}"      
+    }) {
+      name
+    }
+  }
+`;
+
+export const addActivity = createAsyncThunk(
+  'activity/add-activity',
+  async () => {
+    const response = await axios.post('https://qwikle-server.eddi.cloud/', {
+      query: addActivityQuery,
+    });
+    return response;
+  }
+);
 
 const activityReducer = createReducer(initialState, (builder) => {
   builder.addCase(addActivity, (state, action) => {
