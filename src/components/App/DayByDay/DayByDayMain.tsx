799168d7d965ba10ff295c65847259b1a28f4ea1
Laura Feugene <laura.feugene.gmail.com>
2023-06-23 12:03:12 +0200

    :feat: ajout du bouton ajouter / editer les activités

--- src/components/App/DayByDay/DayByDayMain.tsx
+++ src/components/App/DayByDay/DayByDayMain.tsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import { ActivityProps } from '../../../store/reducers/activities';
 
 function DayByDayMain(props: ActivityProps) {
   const { activities } = props;
+  const [newActivity, setNewActivity] = useState('');
+  const [editActivity, setEditActivity] = useState('');
+  const [deleteActivity, setDeleteActivity] = useState('');
+
+  function handleAddActivity(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewActivity(event.target.value);
+  }
+
+  function handleEditActivity(event: React.ChangeEvent<HTMLInputElement>) {
+    setEditActivity(event.target.value);
+  }
+
+  function handleDeleteActivity(event: React.ChangeEvent<HTMLInputElement>) {
+    setDeleteActivity(event.target.value);
+  }
 
   return (
     <div className="flex md:flex-col w-full sm:flex-col">
       {/* contenant le budget du voyage */}
       <div className="flex flex-1 flex-col md:w-full sm:w-full m-2">
         <div className="grid h-20 card bg-lightest rounded-box place-items-center m-1">
-          <h1> Vous avez dépensé : XYU € </h1>
+          <h1> Vous avez dépensé : 1 000 000 000 € </h1>
         </div>
       </div>
       {/* contenant les activités du jour et les prochaines destinations activités du jour et étapes du voyage ce jour */}
@@ -27,30 +43,40 @@ function DayByDayMain(props: ActivityProps) {
             ))}
 
           <div className="">
-            <button className="btn btn-darkest bg-darkest text-lightest w-full hover:bg-lightest hover:text-darkest">
+            <input
+              type="text"
+              placeholder="Ajouter une nouvelle activité"
+              value={newActivity}
+              onChange={handleAddActivity}
+            />
+            <button
+              type="submit"
+              className="btn btn-darkest bg-darkest text-lightest w-full hover:bg-lightest hover:text-darkest"
+              onClick={handleAddActivity}
+            >
               Ajouter une activité
             </button>
           </div>
         </div>
+      </div>
 
-        {/* Prochaines destinations */}
+      {/* Prochaines destinations */}
 
-        <div className="flex flex-col w-full border-opacity-50 m-1">
-          {activities &&
-            activities.map((activity, index) => (
-              <div
-                key={index}
-                className="grid h-20 card bg-lightest rounded-box place-items-center m-1"
-              >
-                {activity.location}
-              </div>
-            ))}
+      <div className="flex flex-col w-full border-opacity-50 m-1">
+        {activities &&
+          activities.map((activity, index) => (
+            <div
+              key={index}
+              className="grid h-20 card bg-lightest rounded-box place-items-center m-1"
+            >
+              {activity.location}
+            </div>
+          ))}
 
-          <div className="">
-            <button className="btn btn-darkest bg-darkest text-lightest w-full hover:bg-lightest hover:text-darkest">
-              Ajouter une étape
-            </button>
-          </div>
+        <div className="">
+          <button className="btn btn-darkest bg-darkest text-lightest w-full hover:bg-lightest hover:text-darkest">
+            Ajouter une étape
+          </button>
         </div>
       </div>
     </div>
