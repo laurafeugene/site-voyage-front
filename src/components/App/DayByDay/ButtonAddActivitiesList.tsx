275d563a96ce1444b037846cb2635f8ada995086
Laura Feugene <laura.feugene.gmail.com>
2023-06-25 21:23:45 +0200

    :add flex to organize

--- src/components/App/DayByDay/ButtonAddActivitiesList.tsx
+++ src/components/App/DayByDay/ButtonAddActivitiesList.tsx
@@ -40,10 +40,9 @@ function AddActivitiesTable() {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="card w-96 bg-lightest shadow-xl">
+    <div className="container p-4 flex flex-col w-full lg:flex-row">
+      <div className="card bg-lightest shadow-xl place-items-center">
         <div className="card-body">
-          <h2 className="card-title">Nouvelle activité</h2>
           <form onSubmit={handleSubmit} className="mb-4">
             <div className="mb-4">
               <input
@@ -116,62 +115,44 @@ function AddActivitiesTable() {
           </form>
         </div>
       </div>
-      <h3 className="mb-6 ml-3 text-2xl font-bold text-darkest-700 dark:text-darkest-300">
-        Aujourd'hui, vous avez prévu : {listItems.length} activités
-      </h3>
-
-      <ol className="border-l-2 border-lightest">
+      <div className="divider lg:divider-horizontal" />
+      <ol className=" grid flex-grow place-items-center">
+        {/* <h3 className="mb-6 ml-3 text-2xl font-bold text-darkest-700 dark:text-darkest-300">
+          Aujourd'hui, vous avez prévu : {listItems.length} activités
+        </h3> */}
         {listItems.map((item, index) => (
           <>
             <li key={index}>
               <div className="flex-start md:flex">
-                <div className="-ml-[13px] flex h-[25px] w-[25px] items-center justify-center rounded-full bg-lightest-100 text-lightest-700">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="h-4 w-4"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                <div className="bg-lightest shadow-xl place-items-center" />
+                <div className="mb-4 flex justify-between">
+                  <p className="text-xl text-darkest hover:text-lightest-600 focus:text-lightest-600 active:text-lightest-700">
+                    {item.title}
+                  </p>
                 </div>
-                <div className="mb-10 ml-6 block max-w-md rounded-lg bg-darkest-50 p-6 shadow-md shadow-black/5 darkest:bg-darkest-700 darkest:shadow-black/10">
-                  <div className="mb-4 flex justify-between">
-                    <a
-                      href="#!"
-                      className="text-sm text-lightest transition duration-150 ease-in-out hover:text-lightest-600 focus:text-lightest-600 active:text-lightest-700"
-                    >
-                      {item.title}
-                    </a>
-                  </div>
-                  <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
-                    {item.members} participants
-                  </p>
-                  <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
-                    {item.location}
-                  </p>
-                  <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
-                    Date : {item.date}
-                  </p>
-                  <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
-                    Heure : {item.time}
-                  </p>
-                  <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
-                    {item.price} €
-                  </p>
-                  <button
-                    type="button"
-                    className="inline-block rounded bg-lightest px-4 pb-[5px] pt-[6px] text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-lightest-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-lightest-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-lightest-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] darkest:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] darkest:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] darkest:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] darkest:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]"
-                    data-te-ripple-init
-                    data-te-ripple-color="bg-light"
-                  >
-                    Modifier
-                  </button>
-                </div>
+                <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
+                  {item.members} participants
+                </p>
+                <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
+                  {item.location}
+                </p>
+                <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
+                  Date : {item.date}
+                </p>
+                <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
+                  Heure : {item.time}
+                </p>
+                <p className="mb-6 text-darkest-700 darkest:text-darkest-200">
+                  {item.price} €
+                </p>
+                <button
+                  type="button"
+                  className="inline-block rounded bg-lightest px-4 pb-[5px] pt-[6px] text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-lightest-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-lightest-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-lightest-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] darkest:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] darkest:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] darkest:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] darkest:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]"
+                  data-te-ripple-init
+                  data-te-ripple-color="bg-light"
+                >
+                  Modifier
+                </button>
               </div>
             </li>
           </>
