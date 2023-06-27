0c69b89ac442575669b352049babc7f4bfcfb244
Laura Feugene <laura.feugene.gmail.com>
2023-06-27 13:25:24 +0200

    :fix: fix header, connect api to General Travel

--- src/components/App/GeneralTravel/GeneralTravelMain.tsx
+++ src/components/App/GeneralTravel/GeneralTravelMain.tsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import dayjs from 'dayjs';
+import { useAppDispatch } from '../../../hooks/redux';
+import { getActivityByDate } from '../../../store/reducers/activities';
+import AddActivitiesTable from '../DayByDay/ButtonAddActivitiesList';
+
 function GeneralTravelMain() {
+  const [activities, setActivities] = useState();
+  const dispatch = useAppDispatch();
+  const voyageId = Number(useParams().voyage);
+  const date = useParams().jour;
+  useEffect(() => {
+    getActivityByDate({
+      date: dayjs(date).format('YYYY-MM-DD'),
+      travelId: voyageId,
+    }).then((res) => {
+      setActivities(res);
+    });
+  }, [dispatch, voyageId, date]);
+
   return (
     <div className="flex w-full sm:flex-col space-x-1">
       <div className="flex md:flex-row w-full sm:flex-col">
@@ -7,55 +27,40 @@ function GeneralTravelMain() {
         <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2">
           {' '}
           {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives au activités dans l'ordre d'arrivée */}
-          <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-            Ski
-          </div>
-          <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-            patin à glace
-          </div>
+          {activities && activities.length > 0 ? (
+            activities.map((activity) => (
+              <div
+                key={activity.id}
+                className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1"
+              >
+                {activity.name}
+              </div>
+            ))
+          ) : (
+            <>
+              <div className="flex flex-col items-center bg-lightest rounded-md p-2 text-darkest text-xl overflow-hidden justify-center mb-10">
+                Il est l'heure de commencer à prévoir vos prochaines activités !
+              </div>
+              <AddActivitiesTable />
+            </>
+          )}
         </div>
-
-        {/* contenant le budget du voyage */}
-        {/* 
-                <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2"> 
-                    <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">Budget avec décompte</div>
-                </div>
- */}
 
         {/* contenant les prochaines étapes du voyage */}
 
-        <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2">
+        <div>
           {' '}
           {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives aux prochaines étapes (itinéraires) du voyage */}
-          <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-            Alpes
-          </div>
-          <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-            Genève
-          </div>
-        </div>
-      </div>
-
-      {/* carrousel des photos souvenirs du voyage */}
-
-      {/* contenant le budget du voyage */}
-
-      <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2">
-        <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-          Budget avec décompte
-        </div>
-      </div>
-
-      {/* contenant les prochaines étapes du voyage */}
-
-      <div className="flex flex-1 flex-col md:w-1/3 sm:w-full m-2">
-        {' '}
-        {/* récupérer les données de la base de données  pour faire le nombre de bulles relatives aux prochaines étapes (itinéraires) du voyage */}
-        <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-          Alpes
-        </div>
-        <div className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1">
-          Genève
+          {activities && activities.length > 0
+            ? activities.map((activity) => (
+                <div
+                  key={activity.id}
+                  className="grid h-20 card bg-lightest-400 rounded-box place-items-center m-1"
+                >
+                  {activity.location}
+                </div>
+              ))
+            : null}
         </div>
       </div>
     </div>
