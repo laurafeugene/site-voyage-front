96e06c8f03e357ec20bcfa80e39e12460a6050a0
Laura Feugene <laura.feugene.gmail.com>
2023-07-12 09:27:09 +0200

    fix: :art: changer color into banner and style

--- src/components/App/Home/Banner/Banner.tsx
+++ src/components/App/Home/Banner/Banner.tsx
@@ -4,30 +4,30 @@ function Banner() {
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Understand User Flow.
+            Envie de vacances ?
             <strong className="font-extrabold text-bittersweet sm:block">
-              Increase Conversion.
+              Organisez vos voyages !
             </strong>
           </h1>
 
           <p className="mt-4 sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+            Plateforme collaborative pour organiser vos voyages en groupe en
+            quelques clics !
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-bittersweet px-12 py-3 text-sm font-medium text-white shadow hover:bg-bittersweet-600 focus:outline-none focus:ring active:bg-bittersweet-500 sm:w-auto"
+              className="block w-full rounded-sm bg-bittersweet px-12 py-3 text-sm font-medium text-white shadow hover:bg-bittersweet-500 focus:outline-none focus:ring active:bg-bittersweet-500 sm:w-auto"
               href="/get-started"
             >
-              Get Started
+              C'est parti !
             </a>
 
             <a
-              className="block w-full rounded px-12 py-3 text-sm font-medium text-bittersweet shadow hover:text-bittersweet-500 focus:outline-none focus:ring active:text-bittersweet-500 sm:w-auto"
+              className="block w-full rounded-sm px-12 py-3 border-2 border-bittersweet text-sm font-medium text-bittersweet shadow hover:text-bittersweet-500 focus:outline-none focus:ring active:text-bittersweet-500 sm:w-auto"
               href="/about"
             >
-              Learn More
+              En savoir plus
             </a>
           </div>
         </div>
