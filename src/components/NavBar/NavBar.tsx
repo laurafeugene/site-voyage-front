523d69f65111f242d5f6cb424a43f700c2db0b4b
Laura Feugene <laura.feugene.gmail.com>
2023-07-19 22:48:15 +0200

    :feat: modification sur la navbar lorsqu'on est connecté ou non. Ajout du state logged et logout

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { logOut } from '../../store/reducers/user';
 
 function NavBar() {
   const [isOpen, setIsOpen] = React.useState(false);
+  const dispatch = useAppDispatch();
+
+  // Pour savoir si l'utilisateur est connecté ou non
+  const isLogged = useAppSelector((state) => state.user.isLogged);
+
+  // L'utilisateur souhaite se déconnecter
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
+
+  let navigation = [{ name: 'Accueil', href: '/' }];
+  if (isLogged) {
+    navigation = [
+      { name: 'Accueil', href: '/' },
+      { name: 'Mes voyages', href: '/voyages' },
+      { name: 'Mon compte', href: `/mon-compte` },
+    ];
+  } else {
+    navigation = [
+      { name: 'Accueil', href: '/' },
+      { name: 'Inscription', href: '/signup' },
+      { name: 'Connexion', href: '/signin' },
+    ];
+  }
 
   return (
-    <nav x-data={{ isOpen }} className="relative dark:bg-gray-800 pb-5">
+    <div x-data={{ isOpen }} className="relative dark:bg-gray-800 pb-5">
       <div className="container px-6 py-4 mx-auto">
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
@@ -76,71 +103,29 @@ function NavBar() {
             } absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center`}
           >
             <div className="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
-              <a
-                href="/"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 font-medium transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-green dark:hover:bg-gray-700"
-              >
-                Accueil
-              </a>
-              <a
-                href="/signup"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 font-medium transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-green dark:hover:bg-gray-700"
-              >
-                Inscription
-              </a>
-              <a
-                href="/signin"
-                className="px-3 py-2 mx-3 mt-2 text-gray-700 font-medium transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-green dark:hover:bg-gray-700"
-              >
-                Connexion
-              </a>
-            </div>
-
-            <div className="flex items-center mt-4 lg:mt-0">
-              <button
-                type="button"
-                className="hidden mx-4 text-gray-600 transition-colors duration-300 transform lg:block dark:text-gray-200 hover:text-gray-700 font-medium dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none"
-                aria-label="show notifications"
-              >
-                <svg
-                  className="w-6 h-6"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+              {navigation.map((category) => (
+                <NavLink
+                  key={category.name}
+                  to={category.href}
+                  className="px-3 py-2 mx-3 mt-2 text-gray-700 font-medium transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-green dark:hover:bg-gray-700"
                 >
-                  <path
-                    d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                    stroke="#20b970"
-                    fill="#20b970"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
-
-              <button
-                type="button"
-                className="flex items-center focus:outline-none"
-                aria-label="toggle profile dropdown"
-              >
-                <div className="w-8 h-8 overflow-hidden border-2 border-green rounded-full">
-                  <img
-                    src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-                    className="object-cover w-full h-full"
-                    alt="avatar"
-                  />
-                </div>
-
-                <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">
-                  Laura François-Eugene
-                </h3>
-              </button>
+                  {category.name}
+                </NavLink>
+              ))}
+              {isLogged && (
+                <NavLink
+                  to="/signin"
+                  onClick={handleLogout}
+                  className="px-3 py-2 mx-3 mt-2 text-gray-700 font-medium transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-green dark:hover:bg-gray-700"
+                >
+                  Se déconnecter
+                </NavLink>
+              )}
             </div>
           </div>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
 
